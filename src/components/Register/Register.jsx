febeaf10742f4525d5611d4f77fc4d769a32a963
Mustafa KAÇAR <mustafakacar48@outlook.com.tr>
2023-12-07 14:29:14 +0300

    Layout changed and some page drafts added

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,34 +1,97 @@
+import { useState } from "react";
 import "./Register.css";
 
 function Register() {
+  const [userType, setUserType] = useState("freelancer");
   return (
     <>
-      <div>
-        <div className="selection-card">
-        <div>
-            <div className="join-container">
-              <h2>Join as a Freelancer or Employer</h2>
-
-              <div className="category-container">
-                <div className="category">
+      <div className="d-flex justify-content-center">
+        <div className="col-7">
+          <div className="card">
+            <div className="card-body">
+              <h2 className="text-center">Join as a Freelancer or Employer</h2>
+              <div className="p-3">
+                <div class="input-group mb-3">
+                  <span class="input-group-text" id="basic-addon1">
+                    @
+                  </span>
                   <input
-                    type="checkbox"
-                    id="freelancer"
-                    name="category"
-                    value="freelancer"
+                    type="text"
+                    class="form-control"
+                    placeholder="Username"
+                    aria-label="Username"
+                    aria-describedby="basic-addon1"
                   />
-                  <label htmlFor="freelancer">Freelancer</label>
                 </div>
-
-                <div className="category">
+                <div class="input-group mb-3">
+                  <input type="text" class="form-control" placeholder="Email" />
+                </div>
+                <div class="input-group mb-3">
                   <input
-                    type="checkbox"
-                    id="employer"
-                    name="category"
-                    value="employer"
+                    type="password"
+                    class="form-control"
+                    placeholder="Password"
                   />
-                  <label htmlFor="employer">Employer</label>
                 </div>
+                <label>Choose your account type</label>
+                <div className="category-container">
+                  <div class="row">
+                    <div class="col-md-6 col-lg-6 col-sm-6">
+                      <label>
+                        <input
+                          type="radio"
+                          name="userType"
+                          class="card-input-element"
+                          value={"customer"}
+                          onChange={(e) => setUserType(e.target.value)}
+                          checked={userType === "customer"}
+                        />
+
+                        <div class="card card-default userType-card">
+                          <div class="card-header">Customer</div>
+                          <div class="card-body">
+                            {" "}
+                            publish your work offer and reach to freelancers
+                            easily
+                          </div>
+                        </div>
+                      </label>
+                    </div>
+                    <div class="col-md-6 col-lg-6 col-sm-6">
+                      <label>
+                        <input
+                          type="radio"
+                          name="userType"
+                          class="card-input-element"
+                          value={"freelancer"}
+                          onChange={(e) => setUserType(e.target.value)}
+                          checked={userType === "freelancer"}
+                        />
+
+                        <div class="card card-default userType-card">
+                          <div class="card-header">Freelancer</div>
+                          <div class="card-body">
+                            complete active works and earn cryptocurrency{" "}
+                          </div>
+                        </div>
+                      </label>
+                    </div>
+                  </div>
+                </div>
+                <center>
+                  {" "}
+                  <div className="btn btn-success col-12 mt-3">Join us</div>
+                </center>
+
+                <center>
+                  <label className="mt-3">
+                    Do you have an account already?{" "}
+                    <a href="/login" className="fw-bold text-primary">
+                      Click here
+                    </a>
+                    to sign in
+                  </label>
+                </center>
               </div>
             </div>
           </div>
---
+++ src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../../components/Layout/Layout';
+import Login from '../../components/Login/Login';
+
+const LoginPage = () => {
+    return (
+        <Layout>
+            <Login/>
+       </Layout>
+    );
+};
+
+export default LoginPage;
