cc2d3bfd1c42d7ed6c534009c98c7dc37433010d
kochipek <ipekkochisarli@gmail.com>
2023-12-13 20:06:29 +0300

    more page drafts added

--- src/pages/FindFreeLancersPage/FindFreelancersPage.jsx
+++ src/pages/FindFreeLancersPage/FindFreelancersPage.jsx
@@ -1,34 +1,34 @@
-
 import Layout from "../../components/Layout/Layout";
 import "./FindFreelancersPage.css";
 
 function FindFreelancersPage() {
-    const freelancerCard = () => {
-      return (
-        <div className="col-3 mt-2">
-          {" "}
-          <div class="card">
-            <img
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Example_image.svg/600px-Example_image.svg.png"
-              class="card-img-top"
-              alt="..."
-            />
-            <div class="card-body">
-              <p class="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-            </div>
+  const freelancerCard = () => {
+    return (
+      <div className="col-3 mt-2">
+        <div className="card">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Example_image.svg/600px-Example_image.svg.png"
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <p className="card-text">
+              Some quick example text to build on the card title and make up
+              the bulk of the card's content.
+            </p>
+            <p className="card-text">Hourly Rate: $25</p>
+            <button className="btn btn-primary">Hire</button>
           </div>
         </div>
-      );
-    };
+      </div>
+    );
+  };
 
-    const listFreelancers = () => {
-      let list = [];
-      for (let i = 0; i < 5; i++) list.push(freelancerCard());
-      return list;
-    };
+  const listFreelancers = () => {
+    let list = [];
+    for (let i = 0; i < 5; i++) list.push(freelancerCard());
+    return list;
+  };
   return (
     <>
       <Layout>
---
+++ src/pages/PaymentsPage/PaymentsPage.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import './PaymentsPage.css';
+import Layout from '../../components/Layout/Layout';
+
+const paymentsData = [
+  {
+    id: 1,
+    jobTitle: 'React Projesi',
+    jobDescription: 'web uygulamasi',
+    incomingAmount: 500,
+    outgoingAmount: 200,
+  },
+  {
+    id: 2,
+    jobTitle: 'Node.js Projesi',
+    jobDescription: 'backend uygulamasi',
+    incomingAmount: 800,
+    outgoingAmount: 300,
+  },
+  {
+    id: 3,
+    jobTitle: 'React Native Uygulaması',
+    jobDescription: 'mobil uygulama',
+    incomingAmount: 600,
+    outgoingAmount: 150,
+  },
+];
+
+const PaymentsPage = () => {
+  const totalIncoming = paymentsData.reduce((total, payment) => total + payment.incomingAmount, 0);
+  const totalOutgoing = paymentsData.reduce((total, payment) => total + payment.outgoingAmount, 0);
+  const totalBalance = totalIncoming - totalOutgoing;
+
+  return (
+    <Layout>
+        <h1>Payment History</h1>
+      <div className="payments-container">
+        {paymentsData.map((payment) => (
+          <div key={payment.id} className="payment-card">
+            <div className="payment-details">
+              <h2>{payment.jobTitle}</h2>
+              <p>{payment.jobDescription}</p>
+            </div>
+            <div className="payment-amounts">
+              <p>Gelen Para: {payment.incomingAmount} </p>
+              <p>Çıkan Para: {payment.outgoingAmount}</p>
+            </div>
+            <hr />
+          </div>
+        ))}
+
+        <div className="total-balance">
+          <p>Toplam Cüzdan Miktarı: {totalBalance}</p>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default PaymentsPage;
