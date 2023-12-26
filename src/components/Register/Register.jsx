94c7a55864fd752b4789596f4bf1d884acc65c0c
Mustafa KAÇAR <mustafakacar48@outlook.com.tr>
2023-12-26 04:14:45 +0300

    BE implemented for some pages f.e create microtransaction, register etc

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,8 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router";
+import { register } from "../../redux/actions/UserAction";
 import "./Register.css";
 
 function Register() {
-  const [userType, setUserType] = useState("freelancer");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [userType, setUserType] = useState("FREELANCER");
+  const userRegister = useSelector((state) => state.userRegister);
+  const { error, loading, userInfo } = userRegister;
+  const history = useNavigate();
+  useEffect(() => {
+    if (userInfo) {
+      history("/profile");
+    }
+  }, [history, userInfo]);
+
+  const dispatch = useDispatch();
+  const handleRegister = () => {
+    dispatch(register(username, email, password, userType));
+  };
   return (
     <>
       <div className="d-flex justify-content-center">
@@ -10,6 +29,11 @@ function Register() {
           <div className="card">
             <div className="card-body">
               <h2 className="text-center">Join as a Freelancer or Employer</h2>
+              {error && (
+                <div class="alert alert-danger my-2" role="alert">
+                  {error}
+                </div>
+              )}
               <div className="p-3">
                 <div class="input-group mb-3">
                   <span class="input-group-text" id="basic-addon1">
@@ -21,16 +45,27 @@ function Register() {
                     placeholder="Username"
                     aria-label="Username"
                     aria-describedby="basic-addon1"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
                   />
                 </div>
                 <div class="input-group mb-3">
-                  <input type="text" class="form-control" placeholder="Email" />
+                  <input
+                    type="text"
+                    class="form-control"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
                 </div>
                 <div class="input-group mb-3">
                   <input
                     type="password"
                     class="form-control"
                     placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
                   />
                 </div>
                 <label>Choose your account type</label>
@@ -42,9 +77,9 @@ function Register() {
                           type="radio"
                           name="userType"
                           class="card-input-element"
-                          value={"customer"}
+                          value={"CUSTOMER"}
                           onChange={(e) => setUserType(e.target.value)}
-                          checked={userType === "customer"}
+                          checked={userType === "CUSTOMER"}
                         />
 
                         <div class="card card-default userType-card">
@@ -63,9 +98,9 @@ function Register() {
                           type="radio"
                           name="userType"
                           class="card-input-element"
-                          value={"freelancer"}
+                          value={"FREELANCER"}
                           onChange={(e) => setUserType(e.target.value)}
-                          checked={userType === "freelancer"}
+                          checked={userType === "FREELANCER"}
                         />
 
                         <div class="card card-default userType-card">
@@ -80,7 +115,12 @@ function Register() {
                 </div>
                 <center>
                   {" "}
-                  <div className="btn btn-success col-12 mt-3">Join us</div>
+                  <div
+                    className="btn btn-success col-12 mt-3"
+                    onClick={handleRegister}
+                  >
+                    Join us
+                  </div>
                 </center>
 
                 <center>
---
+++ src/redux/actions/MicroTransactionAction.js
@@ -0,0 +1,33 @@
+import { postWithAuth, postWithoutAuth } from "../../api/apiCalls";
+import {
+  MICRO_TRANSACTION_CREATE_FAIL,
+  MICRO_TRANSACTION_CREATE_REQUEST,
+  MICRO_TRANSACTION_CREATE_SUCCESS,
+} from "../../constants/MicroTransactionconstants";
+
+export const createMicroTransaction = (body) => async (dispatch) => {
+  try {
+    dispatch({
+      type: MICRO_TRANSACTION_CREATE_REQUEST,
+    });
+
+    const response = await postWithAuth("/api/v1.0/microtransactions/create", {
+      ...body,
+    });
+    const data = response.data;
+    dispatch({
+      type: MICRO_TRANSACTION_CREATE_SUCCESS,
+      payload: {
+        ...data,
+      },
+    });
+  } catch (error) {
+    dispatch({
+      type: MICRO_TRANSACTION_CREATE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
